5ef9dbcff2333055c7d4f6a188e394652d4acd6e
KhalilMS3 <kmsfouk@hiof.no>
2023-10-26 18:22:23 +0200

    Git init
    Git init

---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,45 @@
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.Assert.*;
+
+
+
+public class LeapYearTest {
+
+    /*
+    As we see here I used '@ParameterizedTest' with '@ValueSoruce' to test several values of years in my tests
+    and avoid many assert-functions
+    * */
+    // ---- Leap years ----
+
+    @ParameterizedTest
+    @ValueSource(ints = {2000, 2020, 2400})
+    public void testLeapYearIsDivisibleBy4AndNotBy100(int year){
+        assertTrue(LeapYear.isLeapYear((year)));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1600, 2000, 2400})
+    public void testLeapYearIsDivisibleBy400(int year){
+        assertTrue(LeapYear.isLeapYear((year)));
+
+    }
+
+    // ---- Non leap years ----
+
+    @ParameterizedTest
+    @ValueSource(ints = {2021, 2022, 2023})
+    public void testNonLeapYearIsNotDivisibleBy4(int year){
+        assertFalse(LeapYear.isLeapYear((year)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1700, 1800, 1900})
+    public void testNonLeapYearIsDivisibleBy100AndNotBy400(int year){
+        assertFalse(LeapYear.isLeapYear(year));
+    }
+
+
+}
